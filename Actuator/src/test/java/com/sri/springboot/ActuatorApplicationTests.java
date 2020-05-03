8d1460c9edc80759f7dac0dde23449503dd666cd
sriprabhup <ece.vetrivel@gmail.com>
2020-05-03 15:07:54 +0530

    Inital Commit with HelloWorld,Tomcat and Actuator

---
+++ Actuator/src/test/java/com/sri/springboot/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ WebServiceTomcat/src/main/java/com/sri/springboot/WebServiceTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.sri.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceTomcatApplication.class, args);
+	}
+
+}
